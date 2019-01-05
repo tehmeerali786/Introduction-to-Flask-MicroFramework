1ff205f4347f9048dd0e1e2d038b193ac4ca0264
Tehmeer Ali Paryani <freelanceali786@gmail.com>
2019-01-05 05:16:35 +0500

    Sections 3 Complete

--- thermos.py
+++ thermos.py
@@ -2,11 +2,33 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+class User:
+	def __init__(self, firstname, lastname):
+		self.firstname = firstname
+		self.lastname = lastname
+
+	def initials(self):
+		return "{}. {}.".format(self.firstname[0], self.lastname[0])
+		
+
+
 @app.route('/')
 @app.route('/index')
 def index():
 
-	return render_template('index.html', title="Title passed from the view to the template", text= ["first", "second", "third"])
+	return render_template('index.html', title="Title passed from the view to the template", user=User("Tehmeer Ali", "Paryani"))
+
+@app.route('/add')
+def add():
+	return render_template('add.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def server_error(e):
+	return render_template('500.html'), 500
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=False)
