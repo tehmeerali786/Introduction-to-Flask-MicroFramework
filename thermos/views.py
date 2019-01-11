04668ce67c4d6d70c115f29ae5e4bb143b5ae9bb
Tehmeer Ali Paryani <freelanceali786@gmail.com>
2019-01-11 21:39:11 +0500

    Seventh Module First Commit)

--- thermos/views.py
+++ thermos/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, abort, request
+from flask import render_template, flash, redirect, url_for, abort, request, abort
 from flask_login import login_required, login_user, logout_user, current_user
 
 from thermos import app, db, login_manager
@@ -30,7 +30,22 @@ def add():
         db.session.commit()
         flash("Stored '{}'".format(description))
         return redirect(url_for('index'))
-    return render_template('add.html', form=form)
+    return render_template('bookmark_form.html', form=form, title="Add a bookmark")
+
+
+@app.route('/edit/<int:bookmark_id>', methods=['GET', 'POST'])
+@login_required
+def edit_bookmark(bookmark_id):
+    bookmark = Bookmark.query.get_or_404(bookmark_id)
+    if current_user != bookmark.user:
+        abort(403)
+    form = BookmarkForm(obj=bookmark)
+    if form.validate_on_submit():
+        form.populate_obj(bookmark)
+        db.session.commit()
+        flash("Started '{}' ".format(bookmark.description))
+        return redirect(url_for('user', username=current_user.username))
+    return render_template('bookmark_form.html', form=form, title="Edit bookmark")
 
 
 @app.route('/user/<username>')
